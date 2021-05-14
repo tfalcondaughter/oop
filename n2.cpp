f2c8b13e6d9081e98fdb5e02b8cc9e1d8ae1e5c3
Tanya <tanya.falcondaughter@gmail.com>
2021-05-14 15:54:28 +0300

    6

--- n2.cpp
+++ n2.cpp
@@ -2,12 +2,41 @@
 #include<vector>
 #include<fstream>
 #include<ctime>
+
 using namespace std;
 
 
-int main(){
+void sort(vector<int> &data) {
+    for (int i = 0; i < data.size(); i++) {
+        for (int j = 0; j < data.size() - 1; j++) {
+            if (data[j] > data[j + 1]) {
+                int n = data[j];
+                data[j] = data[j + 1];
+                data[j + 1] = n;
+            }
+        }
+//        cout << data[i] << endl;
+    }
+}
 
 
+int main() {
+    srand(time(0));
+    int n;
+    cin >> n;
+    vector<int> mas;
+
+    for (int i = 0; i < n; i++) {
+        mas.push_back(rand() % 10);
+    }
+
+    ofstream fout("test.txt");
+
+    for (int i = 0; i < n; i++) {
+        fout << mas[i] << " ";
+    }
+    fout << endl;
+    sort(mas);
 
 
     return 0;
